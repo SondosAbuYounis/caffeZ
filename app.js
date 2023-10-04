8e27b8cf468b1f151a701d13c0a525b4643b2eb3
SondosAbuYounis <abuyounissundos@gmail.com>
2023-10-04 23:41:03 +0300

    added  a new form element

--- app.js
+++ app.js
@@ -1,125 +1,97 @@
 
-let person = prompt("Please enter your name:");
+// let person = prompt("Please enter your name:");
 
-if (person == null || person == "") {
-    text = "User cancelled the prompt.";
-  }
-// let gender = prompt("Are you a: " , "Male, Female");
+// if (person == null || person == "") {
+//     text = "User cancelled the prompt.";
+//   }
 
-// while (gender !==  "male" && gender !==  "Male" && gender !==  "female" && gender !==  "Female") {
-//     gender = prompt("Are you a: " , "Male, Female");
+// function getGender() {
+//     var gender = "";
+  
+//     while (gender.toLowerCase() !== "male" && gender.toLowerCase() !== "female") {
+//       gender = prompt("Are you a: ", "Male, Female");
+
+//     }
+//     return gender;
+//     }
+
+//     var gen=getGender();
+  
+
+//   if ( gen == "male"  ||  gen == "Male" ) {
+//     window.alert("Hello Mr. " + person );
+//     } else if (gen == "female"  ||  gen == "Female"){
+//               window.alert("Hello Mrs. " + person); 
+//              } else {
+//               window.alert ("Hello  " + person);
+//              }
+
+// let typeOfDrink = prompt("would you like a hot or cold drink?", "Hot, Cold");
+// let nameOfDrink = prompt("Please write the name of your drink:");
+
+// let array = [];
+// array.push(person);
+// array.push(gen);
+// array.push(typeOfDrink);
+// array.push(nameOfDrink);
+
+// for (d=0; d< array.length; d++){
+//     console.log(array[d]);
 // }
-function getGender() {
-    var gender = "";
-  
-    while (gender.toLowerCase() !== "male" && gender.toLowerCase() !== "female") {
-      gender = prompt("Are you a: ", "Male, Female");
 
-    }
-    return gender;
-    }
+// ----------------------------------------------------
 
-    var gen=getGender();
-  
+// Info collected using Prompt 
 
-  if ( gen == "male"  ||  gen == "Male" ) {
-    window.alert("Hello Mr. " + person );
-    } else if (gen == "female"  ||  gen == "Female"){
-              window.alert("Hello Mrs. " + person); 
-             } else {
-              window.alert ("Hello  " + person);
-             }
+// let perName = document.createElement("div");
+// perName.id= 'personName';
+// let parag = document.createElement("p");
+// parag.textContent=`${person}`;
+// perName.appendChild(parag);
 
-let typeOfDrink = prompt("would you like a hot or cold drink?", "Hot, Cold");
-let nameOfDrink = prompt("Please write the name of your drink:");
 
-let array = [];
-array.push(person);
-array.push(gen);
-array.push(typeOfDrink);
-array.push(nameOfDrink);
+// let list = document.createElement("ul");
+// perName.appendChild(list);
+// let perGen = document.createElement("li");
+// perGen.textContent=`Gender: ${gen}`;
+// list.appendChild(perGen);
+// let driTy = document.createElement("li");
+// driTy.textContent=`Drink: ${typeOfDrink} ${nameOfDrink}`;
+// list.appendChild(driTy);
 
-for (d=0; d< array.length; d++){
-    console.log(array[d]);
+// document.body.appendChild(perName);
+
+// ----------------------------------------------------
+
+// Info collected using Form 
+var userNameVal = document.getElementById("userName").value;
+var genderVal = document.getElementById("genderVal").value;
+var naDrVal = document.getElementById("naDr").value;
+
+// let userNameVal = document.createElement("li");
+// userNameVal.textContent=`Name: ${userName}`;
+// orderInfoVal.appendChild(userNameVal);
+// let genderVal = document.createElement("li");
+// orderInfoVal.textContent=`Gender: ${genderVal}`;
+// orderInfoVal.appendChild(genderVal);
+// let naDrVal = document.createElement("li");
+// naDrVal.textContent=`Drink: ${naDr}`;
+// orderInfoVal.appendChild(naDrVal);
+
+
+var checkbox = document.getElementById("boxx"); // Get the checkbox element by its ID
+var checkboxa = document.getElementById("boxxa"); // Get the checkbox element by its ID
+var drinkTemp = [];
+if (checkbox.checked) {
+  drinkTyype.push(checkbox.value);
 }
+if (checkboxa.checked) {
+  drinkTyype.push(checkboxa.value);
+} 
 
+var resultText = "Username: " + userNameVal + "<br>";
+resultText += "Gender: " + genderVal + "<br>";
+resultText += "Drink: " + drinkTemperature.join(", ") + naDrVal;
 
-
-
-// const conDi = document.querySelector('#personName');
-// conDi.textContent="hiiiiiii";
-
-let perName = document.createElement("div");
-perName.id= 'personName';
-let parag = document.createElement("p");
-parag.textContent=`${person}`;
-perName.appendChild(parag);
-
-
-let list = document.createElement("ul");
-perName.appendChild(list);
-let perGen = document.createElement("li");
-perGen.textContent=`Gender: ${gen}`;
-list.appendChild(perGen);
-let driTy = document.createElement("li");
-driTy.textContent=`Drink: ${typeOfDrink} ${nameOfDrink}`;
-list.appendChild(driTy);
-// let driNam = document.createElement("li");
-// list.appendChild(driNam);
-
-// document.conDi.appendChild(perName);
-
-
-
-// document.body.insertBefore(perName, container);
-// perName.classList.add('centered');
-document.body.appendChild(perName);
-
-// object.appendChild(perName);
-
-
-
-
-
-// let mmm = document.createElement("div");
-// parag.textContent="Usersname";
-// conDi.appendChild(mmm);
-
-
-
-
-
-
-
-
-
-// if ( gender == "male"  ||  gender == "Male" ) {
-//   window.alert("Hello Mr. " + person + ", your " + drink + " is in the making. "+ "Thank you for your patience =)");
-//   console.log("Mr. " + person + drink);
-//   } else if (gender == "female"  ||  gender == "Female"){
-//             window.alert("Hello Mrs. " + person + ", your " + drink + " is in the making. "+ "Thank you for your patience =)");
-//             console.log("Mrs. " + person + drink);
-    
-//            } else {
-//             window.alert ("Hello  " + person + ", your  " + drink + " is in the making. "+ "Thank you for your patience =)");
-//             console.log( person + drink);  
-//            }
-
-// if (drink == null || drink == ""){
-//     window.alert ("Invalid order");
-// } else {
-//     if ( gender == "male"  ||  gender == "Male" ) {
-//         window.alert("Hello Mr. " + person + ", your " + drink + " is in the making. "+ "Thank you for your patience =)");
-//         console.log("Mr. " + person + drink);
-//      } else if (gender == "female"  ||  gender == "Female"){
-//         window.alert("Hello Mrs. " + person + ", your " + drink + " is in the making. "+ "Thank you for your patience =)");
-//         console.log("Mrs. " + person + drink);
-
-//      } else {
-//         window.alert ("Hello  " + person + ", your  " + drink + " is in the making. "+ "Thank you for your patience =)");
-//         console.log( person + drink);
-
-//      }
-
-// }
+document.getElementById("orderInfoVal").innerHTML = resultText;
 
